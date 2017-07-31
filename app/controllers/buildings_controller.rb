cc311256f8f408ca7f0c6ccee9b6f3a6126d003b
Janice Wong <JaniceWong13@gmail.com>
2017-07-31 17:44:08 -0400

    Rebased to earlier commit

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,7 +1,7 @@
 class BuildingsController < ApplicationController
   def index
-    @locations = Energy.uniq.pluck(:location)
-    @measurements = Energy.uniq.pluck(:measurement)
+    @locations = Energy.distinct.pluck(:location)
+    @measurements = Energy.distinct.pluck(:measurement)
     @years = ['2008', '2009', '2010', '2011', '2012']
     if params[:year]
       @year = params[:year]
